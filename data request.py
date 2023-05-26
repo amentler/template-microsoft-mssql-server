92e5fafd61c87038f230bfd08da22438baaec012
amentler <amentler+github@gmail.com>
2023-05-26 16:22:19 +0000

    fixed issues. added ipynb notebook for jupyter notebook.

--- data request.py
+++ data request.py
@@ -1,10 +1,5 @@
 import pyodbc 
 
-server = 'tcp:localhost' 
-database = 'master' 
-username = 'sa' 
-password = 'TeSt1234' 
-# ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
 cnxn = pyodbc.connect("DRIVER={ODBC Driver 18 for SQL Server};SERVER=localhost;DATABASE=master;ENCRYPT=yes;UID=sa;PWD=TeSt1234;TrustServerCertificate=yes;")
 cursor = cnxn.cursor()
 
--- notebook.sql
+++ notebook.sql
@@ -1,3 +1,12 @@
+/*use rightclick -> open with   to run as a notebook!
+config notebook on sql notebook tab:
+host: localhost
+port: 1433
+user: sa
+database: master
+driver: mssql
+*/
+
 SELECT name FROM sys.databases
 
 use master
