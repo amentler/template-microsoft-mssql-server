f7ab63180ea5e3d28d076b2ac6ccb23dc743f182
Mentler, André <amentler@brockhaus-ag.de>
2024-02-01 18:33:59 +0100

    extended for local sql training

---
+++ vscode_container/recreate.ps1
@@ -0,0 +1,7 @@
+﻿docker compose down
+docker compose build --no-cache #--progress plain
+docker compose up -d
+start-sleep -seconds 1
+Start-Process "http://localhost:8080"
+$containerId = docker ps --filter "ancestor=vscode_container-code-server" --format "{{.ID}}"
+docker exec -it $containerId bash
---
+++ vscode_container/workspace/mssql.session.sql
@@ -0,0 +1,8 @@
+use test
+drop table blubb
+create table blubb (
+    first int,
+    second VARCHAR(20)
+)
+
+select * from blubb
